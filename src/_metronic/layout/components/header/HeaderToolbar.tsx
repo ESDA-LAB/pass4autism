f15c651d98df13d5999ef076180a40984da92536
d.kontargiris <kondargiris@gmail.com>
2024-09-12 19:01:29 +0300

    header toolbar Removals and cleanup

--- src/_metronic/layout/components/header/HeaderToolbar.tsx
+++ src/_metronic/layout/components/header/HeaderToolbar.tsx
@@ -89,25 +89,11 @@ const HeaderToolbar = () => {
           {/* begin::Action wrapper */}
           <div className='d-flex align-items-center'>
             {/* begin::Label */}
-            <span className='fs-7 fw-bolder text-gray-700 pe-4 text-nowrap d-none d-xxl-block'>
-              Sort By:
-            </span>
+
             {/* end::Label */}
 
             {/* begin::Select */}
-            <select
-              className='form-select form-select-sm form-select-solid w-100px w-xxl-125px'
-              data-control='select2'
-              data-placeholder='Latest'
-              data-hide-search='true'
-              defaultValue={status}
-              onChange={(e) => setStatus(e.target.value)}
-            >
-              <option value=''></option>
-              <option value='1'>Latest</option>
-              <option value='2'>In Progress</option>
-              <option value='3'>Done</option>
-            </select>
+
             {/* end::Select  */}
           </div>
           {/* end::Action wrapper */}
@@ -115,30 +101,14 @@ const HeaderToolbar = () => {
           {/* begin::Action wrapper */}
           <div className='d-flex align-items-center'>
             {/* begin::Separartor */}
-            <div className='bullet bg-secondary h-35px w-1px mx-5'></div>
+
             {/* end::Separartor */}
 
             {/* begin::Label */}
-            <span className='fs-7 text-gray-700 fw-bolder d-none d-sm-block'>
-              Impact <span className='d-none d-xxl-inline'>Level</span>:
-            </span>
+
             {/* end::Label */}
 
             {/* begin::NoUiSlider */}
-            <div className='d-flex align-items-center ps-4' id='kt_toolbar'>
-              <div
-                id='kt_toolbar_slider'
-                className='noUi-target noUi-target-primary w-75px w-xxl-150px noUi-sm noUi-ltr noUi-horizontal noUi-txt-dir-ltr'
-              ></div>
-
-              <span
-                id='kt_toolbar_slider_value'
-                className='d-flex flex-center bg-light-primary rounded-circle w-35px h-35px ms-4 fs-7 fw-bolder text-primary'
-                data-bs-toggle='tooltip'
-                data-bs-placement='top'
-                title='Set impact level'
-              ></span>
-            </div>
             {/* end::NoUiSlider */}
 
             {/* begin::Separartor */}
@@ -158,14 +128,7 @@ const HeaderToolbar = () => {
             {/* begin::Actions */}
             <div className='d-flex'>
               {/* begin::Action */}
-              <a
-                href='#'
-                className='btn btn-sm btn-icon btn-icon-muted btn-active-icon-primary'
-                data-bs-toggle='modal'
-                data-bs-target='#kt_modal_invite_friends'
-              >
-                <KTSVG path='/media/icons/duotune/files/fil003.svg' className='svg-icon-1' />
-              </a>
+
               {/* end::Action */}
 
               {/* begin::Notifications */}
@@ -181,9 +144,7 @@ const HeaderToolbar = () => {
               {/* begin::Quick links */}
               <div className='d-flex align-items-center'>
                 {/* begin::Menu wrapper */}
-                <a href='#' className='btn btn-sm btn-icon btn-icon-muted btn-active-icon-primary'>
-                  <KTSVG path='/media/icons/duotune/files/fil010.svg' className='svg-icon-1' />
-                </a>
+
                 {/* end::Menu wrapper */}
               </div>
               {/* end::Quick links */}
