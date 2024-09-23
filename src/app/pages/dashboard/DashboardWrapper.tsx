c163df03eb35e0c2bd206fab3c137a373c94ef1f
d.kontargiris <kondargiris@gmail.com>
2024-09-23 20:06:58 +0300

    align dashboard

--- src/app/pages/dashboard/DashboardWrapper.tsx
+++ src/app/pages/dashboard/DashboardWrapper.tsx
@@ -19,23 +19,6 @@ const DashboardPage = () => (
   <>
     {/* begin::Row */}
     <div className='row g-5 g-xl-8'>
-      <div className='col-xl-4'>
-        <button onClick={() => window.open('https://pass4autism-project.eu/', '_blank')}
-                style={{background: 'none', border: 'none', padding: '0', cursor: 'pointer'}}>
-          <StatisticsWidget5
-            className="card-xl-stretch mb-xl-8"
-            svgIcon="/media/icons/duotune/electronics/elc001.svg"
-            color="body-white"
-            iconColor="primary"
-            title="Visit our Website"
-            description="Learn more about the project - PASS4Autism"
-            titleColor="gray-900"
-            descriptionColor="gray-400"
-          />
-        </button>
-
-      </div>
-
       <div className="col-xl-4">
         <button onClick={() => window.open('https://pass4autism-project.eu/', '_blank')}
                 style={{background: 'none', border: 'none', padding: '0', cursor: 'pointer'}}>
@@ -49,7 +32,7 @@ const DashboardPage = () => (
             titleColor="white"
             descriptionColor="white"
           />
-          </button>
+        </button>
       </div>
 
       <div className="col-xl-4">
@@ -67,78 +50,34 @@ const DashboardPage = () => (
           />
         </button>
       </div>
+
+      <div className='col-xl-4'>
+        <button onClick={() => window.open('https://pass4autism-project.eu/', '_blank')}
+                style={{background: 'none', border: 'none', padding: '0', cursor: 'pointer'}}>
+          <StatisticsWidget5
+            className="card-xl-stretch mb-xl-9"
+            svgIcon="/media/icons/duotune/electronics/elc001.svg"
+            color="body-white"
+            iconColor="primary"
+            title="Visit our Website"
+            description="Learn more about the project - PASS4Autism"
+            titleColor="gray-900"
+            descriptionColor="gray-400"
+          />
+        </button>
+
+      </div>
     </div>
     {/* end::Row */}
 
     {/* begin::Row */}
     <div className="row g-5 g-xl-8">
       {/* begin::Col */}
-      <div className="col-xl-4">
-        <ListsWidget1 className='card-xl-stretch mb-xl-8' />
-      </div>
-      {/* end::Col */}
-
-      {/* begin::Col */}
-      <div className='col-xl-8'>
+      <div className='col-xl-12'>
         <TablesWidget5 className='card-xl-stretch mb-5 mb-xl-8' />
       </div>
       {/* end::Col */}
     </div>
-    {/* end::Row */}
-
-    {/* begin::Row */}
-    <div className='row gy-5 g-xl-8'>
-      <div className='col-xxl-4'>
-        <MixedWidget2
-          className='card-xl-stretch mb-xl-8'
-          chartColor='danger'
-          chartHeight='200px'
-          strokeColor='#cb1e46'
-        />
-      </div>
-      <div className='col-xxl-4'>
-        <ListsWidget5 className='card-xxl-stretch' />
-      </div>
-      <div className='col-xxl-4'>
-        <MixedWidget10
-          className='card-xxl-stretch-50 mb-5 mb-xl-8'
-          chartColor='primary'
-          chartHeight='150px'
-        />
-        <MixedWidget11
-          className='card-xxl-stretch-50 mb-5 mb-xl-8'
-          chartColor='primary'
-          chartHeight='175px'
-        />
-      </div>
-    </div>
-    {/* end::Row */}
-
-    {/* begin::Row */}
-    <div className='row gy-5 gx-xl-8'>
-      <div className='col-xxl-4'>
-        <ListsWidget3 className='card-xxl-stretch mb-xl-3' />
-      </div>
-      <div className='col-xl-8'>
-        <TablesWidget10 className='card-xxl-stretch mb-5 mb-xl-8' />
-      </div>
-    </div>
-    {/* end::Row */}
-
-    {/* begin::Row */}
-    <div className='row gy-5 g-xl-8'>
-      <div className='col-xl-4'>
-        <ListsWidget2 className='card-xl-stretch mb-xl-8' />
-      </div>
-      <div className='col-xl-4'>
-        <ListsWidget6 className='card-xl-stretch mb-xl-8' />
-      </div>
-      <div className='col-xl-4'>
-        <ListsWidget4 className='card-xl-stretch mb-5 mb-xl-8' items={5} />
-        {/* partials/widgets/lists/_widget-4', 'class' => 'card-xl-stretch mb-5 mb-xl-8', 'items' => '5' */}
-      </div>
-    </div>
-    {/* end::Row */}
   </>
 )
 
