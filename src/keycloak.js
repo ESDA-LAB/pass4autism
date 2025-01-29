ae9f553a36e201bb6d80e349b6b8038e21aaa850
Ioannis Christodoulopoulos <ichristod@cti.gr>
2025-01-29 18:46:24 +0200

    Update keycloak url (from private to public)

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'https://10.10.20.17:8443', // URL του Keycloak server
+  url: 'https://195.251.8.195:8443', // URL του Keycloak server
   realm: 'pass4autism',              // Το όνομα του Realm
   clientId: 'pass4autism-localhost-frontend',     // Το ID του client στο Keycloak
   enableLogging: true, // Προαιρετικό για debugging
